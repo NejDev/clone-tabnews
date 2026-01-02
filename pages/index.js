765f566848b3e6add40ae9c27e6df3e3732b03a0
NejDev <123705901+NejDev@users.noreply.github.com>
2026-01-02 14:24:19 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Hello World! A new start to a dreaming mind :D </h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
